87dd08574f32739f6f716c83c7dc1c018e91a60f
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2024-07-13 00:50:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+
+inquirer
+  .prompt([{
+    message: "Type in your URL: ",
+    name: "URL"
+  }])
+  .then((answers) => {
+    const url = answers.URL;
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
