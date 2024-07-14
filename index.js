2a6437b0b78c55f9f5d1dc41f1e9f2272be469cb
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2024-07-14 09:50:46 +0530

    text file functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
+import { log } from "console";
 
 inquirer
   .prompt([
@@ -13,11 +14,16 @@ inquirer
     const url = answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("URL.txt", url, (err) =>{
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+      console.error("Error: Prompt couldn't be rendered in the current environment.");
     } else {
-      // Something else went wrong
+      console.error("An unexpected error occurred:", error);
     }
   });
